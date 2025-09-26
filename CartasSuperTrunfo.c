9df169ad2089441fbfc3194e0e51136884fa8f1b
EdCE7 <141381597+EdCE7@users.noreply.github.com>
2025-09-26 04:06:18 +0000

    Começo do projeto, imprime variáveis e informações colocadas pelo usuário

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,41 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+char estado;
+char codigo[3], cidade[50];
+int populacao, pontos_tu;
+float area, pib;
   // Área para entrada de dados
+printf("Qual o seu Estado?\n");
+scanf("%c", &estado);
+
+printf("Qual o código da carta?\n");
+scanf("%s", codigo);
+
+printf("Qual o nome da cidade?\n");
+scanf("%s", &cidade);
+
+printf("Qual a população da cidade?\n");
+scanf("%d", &populacao);
+
+printf("Qual a área da cidade?\n");
+scanf("%f", &area);
+
+printf("Qual o PIB da cidade?\n");
+scanf("%f", &pib);
+
+printf("Quantos pontos turísticos há na cidade?\n");
+scanf("%d", &pontos_tu);
+
 
   // Área para exibição dos dados da cidade
+printf("Estado: %c\n", estado);
+printf("Código: %s\n", codigo);
+printf("Cidade: %s\n", cidade);
+printf("População: %d\n", populacao);
+printf("Area: %f\n", area);
+printf("PIB: %f\n", pib);
+printf("Pontos Turísticos: %d\n", pontos_tu);
 
 return 0;
 } 
